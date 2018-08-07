2b5c64ef670cc22226bd34aab20b8b424ca90562
Moshe Fromer <mofromer@gmail.com>
2018-08-07 11:11:06 +0300

    reduce ex2 after understanding the point

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const items = [4,8,5,9];
-const mult = items.reduce((acc,val) => acc * val,1);
+const mult = reduce(items,(acc,val) => acc * val,1);
 console.log(mult);
 
 $('#click').on('click',function(){
@@ -8,3 +8,7 @@ $('#click').on('click',function(){
 
 
 $('#click').off('click');
+
+function reduce(array,callback,initial){
+    return array.reduce(callback,initial);
+}
